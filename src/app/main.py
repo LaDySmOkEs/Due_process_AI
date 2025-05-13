2d13af67c82b8860ba13f3cccd36e44d1162591a
LaDySmOkEs <panterailovephil@gmail.com>
2025-05-12 18:10:19 -0700

    Update main.py

--- src/app/main.py
+++ src/app/main.py
@@ -1,12 +1,4 @@
-from flask import Flask, render_template
-import logging
-
-app = Flask(__name__)
-logging.basicConfig(level=logging.DEBUG)
-
-@app.route("/")
-def home():
-    return render_template("home.html")
+from app import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
